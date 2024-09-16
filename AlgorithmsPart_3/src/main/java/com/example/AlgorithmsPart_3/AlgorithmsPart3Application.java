c7e52bbe323489bc0ee3913bf79a0499eb04f6d8
Pavel <pavelpulle1@gmail.co>
2024-09-16 23:40:33 +0300

    Initial commit

---
+++ AlgorithmsPart_3/src/main/java/com/example/AlgorithmsPart_3/AlgorithmsPart3Application.java
@@ -0,0 +1,13 @@
+package com.example.AlgorithmsPart_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsPart3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmsPart3Application.class, args);
+	}
+
+}
---
+++ AlgorithmsPart_3/src/test/java/com/example/AlgorithmsPart_3/AlgorithmsPart3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AlgorithmsPart_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmsPart3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
